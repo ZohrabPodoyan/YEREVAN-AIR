995fa66ce1ba64851f2069b34380e70e013ab5da
ZohrabPodoyan <podoyan_zoro@mail.ru>
2026-03-23 05:33:00 +0400

    fix: config reads from env variables, safe to commit

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+OWM_KEY    = os.environ["OWM_KEY"]
+OPENAQ_KEY = os.environ["OPENAQ_KEY"]
+
+LAT_CENTER  = 40.1792
+LON_CENTER  = 44.5133
+OUTPUT_FILE = "yerevan_air.html"
+
+DT              = 300
+DECAY           = 0.94
+DIFFUSION       = 0.0004
+MAX_PARTICLES   = 600
+FORECAST_STEPS  = 12
+ALERT_THRESHOLD = 100
+
+YEREVAN_STATION_IDS = [2960649, 2960634, 2960632]
